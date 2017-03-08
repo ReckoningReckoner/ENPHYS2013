e32e3ddf7ac1efcd18c9326e5bfade2b2ba24200
Viraj Bangari <Viraj3f@gmail.com>
2017-03-08 12:53:09 -0500

    Finish lab7

--- Lab7/task4b.m
+++ Lab7/task4b.m
@@ -1,10 +1,14 @@
-x=[1,3,4.5,7,8,10]; 
-y=[2,3,3.6,3.8,3.5,3];
-M=3 % cubic poly
+X = [0.01 0.06 0.4 0.9 1.0];
+Y = [0.04 0.1 0.2 0.05 0];
 
-P = fliplr(least_sq(x, y, M)
-xvalues = linspace(1, 10, 20);
+L = 1;
+S=@(a, x) a(1).*(x./L).^(1./2)+a(2).*(x./L)+a(3).*(x./L).^2+a(4).*(x./L).^3+a(5).*(x./L).^4;
+
+P = airfoil(X, Y)
+xvalues = linspace(0, 1, 100);
+
 hold on;
-plot(x, y);
-plot(xvalues, polyval(P, xvalues));
+plot(xvalues, S(P, xvalues));
+plot(xvalues, -S(P, xvalues));
+plot(X, Y, '*');
 hold off;
